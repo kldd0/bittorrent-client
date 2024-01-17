de1bc37cda41ae626e32b0f3fd0c02fea4e04565
Stepan <steparesh@gmail.com>
2024-01-17 17:35:13 +0300

    chore: minor style and structure changes

--- utils/bencode/src/b_integer.cpp
+++
@@ -1,5 +0,0 @@
-#include "../include/bencode/b_integer.hpp"
-
-BInteger::BInteger(long long num)
-  : m_value(num)
-{}
--- utils/bencode/src/b_string.cpp
+++
@@ -1,7 +0,0 @@
-#include <utility>
-
-#include "../include/bencode/b_string.hpp"
-
-BString::BString(std::string str)
-  : m_value(std::move(str))
-{}
